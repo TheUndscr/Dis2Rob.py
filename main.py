c7c523d758a94e369eea6ef0e1521cc67adc7d25
undscr <72475686+TheUndscr@users.noreply.github.com>
2022-03-24 16:51:38 +0000

    - Minor improvements

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
+# IMPORTANT NOTE: DiscordID fetch from RobloxID has not been implemented into the program as of yet. 
+# ANOTHER IMPORTANT NOTE: Sometimes people may link an alternate Roblox account to their Discord account. We can't do much about that.
+
 import requests as http
 
 # Variables
-api = "https://api.blox.link/v1/user" # Bloxlink API
+bloxlinkApi = "https://api.blox.link/v1/user" # Bloxlink API
+erynApi = "https://verify.eryn.io/api/roblox" # Eryn API (for RTD)
 robloxUserList = []
+discordUserList = []
 
-#roblox user fetch
-def fetchId(id):
+# Roblox user fetch
+def fetchRobloxId(id):
   # Fetch the API and store the response inside inf
-  inf = http.get(api + "/{}".format(int(id)))
-
+  inf = http.get(bloxlinkApi + "/{}".format(int(id)))
   if inf.status_code == 200: # If the request was successful:
     # Convert request into a JSON, get the roblox account id,
     # and return
-    fid = inf.json()["primaryAccount"]
-    return fid
+    fid = inf.json()
+    if fid["status"] == "ok":
+      return fid["primaryAccount"]
+    else:
+      print("Error: {}".format(fid["error"]))
+  else:
+    print("Error {}: The fetch pooed itself.".format(inf.status_code))
+    
+# Discord user fetch
+def fetchDiscordId(id):
+  disc = http.get(erynApi + "/{}".format(int(id)))
+  if disc.status_code == 200:
+    did = disc.json()
+    if did["status"] == "ok":
+      return did["users"]
+    else:
+      print("Error: {}".format(did["error"]))
+  else:
+    print("Error {}: Something went wrong.".format(disc.status_code))
 
+    
 # User input processing
 def theFunny():
   userinput = input("> ").split()
@@ -24,7 +46,7 @@ def theFunny():
 # User inputs a valid DiscordID connected to the Bloxlink API to return a RobloxID
   elif userinput[0] == "dtr":
     try:
-      robloxId = fetchId(userinput[1])
+      robloxId = fetchRobloxId(userinput[1])
       print("Roblox ID: " + robloxId)
       print("Roblox Profile: " + " https://www.roblox.com/users/" + robloxId + "/profile")
       
@@ -35,11 +57,20 @@ def theFunny():
   elif userinput[0] == "listusers":
     print("Current roblox IDs: ")
     for id in robloxUserList:
-      print(id)   
+      print(id)
+  elif userinput[0] == "rtd":
+    try:
+      discordId = fetchDiscordId(userinput[1])
+      for id in discordId:
+        print(id)
+        discordUserList.append(id)
+      print("If you wish to lookup the Discord tag associated with this Discord ID, go to this website: " + " https://discord.id/")
+    except:
+      print("Valid Roblox ID not entered. Please try again.")
 # funny
   else:
     print('foo')
-  return theFunny()
 
+  return theFunny()
 # Start the program loop
 theFunny()
